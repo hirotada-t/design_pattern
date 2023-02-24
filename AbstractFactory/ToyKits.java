c8ce37241b783a320f36d716a15615f4f72aafed
Hirotada Takeuchi <t.hir0tada.biz@gmail.com>
2023-02-24 15:47:51 +0900

    feat: machine course parts

--- AbstractFactory/ToyKits.java
+++ AbstractFactory/ToyKits.java
@@ -1,88 +1,134 @@
-class Person {
-  private String firstName;
-  private String lastName;
-  private int age;
+import java.util.HashMap;
 
-  public Person(String firstName, String lastName, int age) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.age = age;
+// Mini4WD
+class Spec {
+  protected int weightG;
+  protected int costYen;
+}
+
+class Structore {
+  protected Parts battery;
+  
+  public Structore() {}
+
+}
+
+class Mini4WD {
+  protected Spec spec;
+  protected Structore structore;
+
+  public Mini4WD(Parts battery) {
+    this.structore = new Structore();
+    this.structore.battery = battery;
   }
 
-  public String getName() {
-    return this.firstName + " " + this.lastName;
+  public int getTotalCosts() {
+    return this.structore.battery.costYen;
   }
-
-  public int getAge() {
-    return this.age;
-  }
-
-  public String toString() {
-    return "My name is " + this.getName() + "(" + this.age + ") .";
+  
+  public int getTotalWeight() {
+    return this.structore.battery.weightG;
   }
 }
 
-// parts interface
-interface Product {
-  public int getCosts();
+class NormalMini4WD extends Mini4WD {
+  public NormalMini4WD(Parts battery) {
+    super(battery);
+  }
 }
-
-// General parts class
-// head
-// body
-// armss
-// legs
-// eyes
-// hands
-// feet
-
-// CyberVampire parts class
-// head
-// body
-// armss
-// legs
-// eyes
-// hands
-// feet
-
-// UnderworldCerberus parts class
-// head
-// body
-// armss
-// legs
-// eyes
-// hands
-// feet
 
 // AbstractFactory
-interface HumanoidToyKitFactory {
-  // FactoryMethod
-  // head
-  // body
-  // armss
-  // legs
-  // eyes
-  // hands
-  // feet
+interface Mini4WDFactory {
+  abstract public Mini4WD createMini4wd();
+  abstract public Parts createBattery();
 }
 
-// CreationMethod
-class CyberVampire implements HumanoidToyKitFactory {
+class NormalMini4WDFactory implements Mini4WDFactory {
+  @Override
+  public Mini4WD createMini4wd() {
+    Parts battery = this.createBattery();
+    return new NormalMini4WD(battery);
+  }
+
+  @Override
+  public Parts createBattery() {
+    return new Battery();
+  }
 }
 
-class UnderworldCerberus implements HumanoidToyKitFactory {
+// Parts
+class Parts {
+  protected int costYen;
+  protected int weightG;
+
+  public Parts(int costYen, int weightG) {
+    this.costYen = costYen;
+    this.weightG = weightG;
+  }
+
+  public int getCost() {
+    return this.costYen;
+  }
+
+  public int getWeight() {
+    return this.weightG;
+  }
+}
+
+class Battery extends Parts {
+  public static final double POWER_W = 1.5;
+
+  public Battery() {
+    super(50, 25);
+  }
+}
+
+// Course
+abstract class CourseAssistant {
+  protected boolean laneChange;
+  protected double upDown;
+  protected int corner;
+  
+  protected CourseAssistant(boolean laneChange, double upDown, int corner) {
+    this.laneChange = laneChange;
+    this.upDown = upDown;
+    this.corner = corner;
+  }
+
+  abstract public void raceMini4WD(Mini4WD machine);
+}
+
+class EasyCourse extends CourseAssistant {
+  public EasyCourse() {
+    super(false, 0, 4);
+  }
+
+  @Override
+  public void raceMini4WD(Mini4WD machine) {
+    System.out.println("easy");
+  }
 }
 
 class FairyWorld {
-  public void createHumanoidToyByKit(HumanoidToyKitFactory factory) {}
+  public HashMap<String, CourseAssistant> courseMap = new HashMap<>();
+  public HashMap<String, Mini4WDFactory> mini4WDMap = new HashMap<>();
+
+  public FairyWorld() {
+    courseMap.put("easy", new EasyCourse());
+
+    mini4WDMap.put("normal", new NormalMini4WDFactory());
+  }
+
+  public void run(String courseKey, String machineKey) {
+    Mini4WD machine = mini4WDMap.get(machineKey).createMini4wd();
+    this.courseMap.get(courseKey).raceMini4WD(machine);
+    System.out.println("test-OK");
+  }
 }
 
 class ToyKits {
   public static void main(String[] args) {
-    Person child = new Person("seiya", "suzuki", 5);
-    Person adult = new Person("masato", "morishita", 25);
-
-    System.out.println(child);
-    System.out.println(adult);
+    FairyWorld fairyWorld = new FairyWorld();
+    fairyWorld.run("easy", "normal");
   }
 }
